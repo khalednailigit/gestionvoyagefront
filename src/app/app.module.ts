4f67bd371ebd49fe22ec8db6add2d252207390ea
Nozha GHARNOUGUI <nozha.gharnougui@esprit.tn>
2022-07-20 21:23:24 +0100

    fix errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
+import { TestComponent } from './pages/test/test.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { ComponentsModule } from './components/components.module';
   declarations: [
     AppComponent,
     AdminLayoutComponent,
-    AuthLayoutComponent
+    AuthLayoutComponent,
+    TestComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
