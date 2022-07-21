c87911c09b6da812f8eeebcb7c5354206f199d7f
Fadhila Boukari <fadhila.boukari@esprit.tn>
2022-07-21 22:28:30 +0200

    ajout feedback

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 import { BlogComponent } from './pages/blog/blog.component';
 import { FeedBackComponent } from './pages/feedbackandreclamation/feed-back/feed-back.component';
+import { RendezVousFeedbackComponent } from './pages/feedbackandreclamation/rendez-vous-feedback/rendez-vous-feedback.component';
 
 
 @NgModule({
@@ -31,7 +32,8 @@ import { FeedBackComponent } from './pages/feedbackandreclamation/feed-back/feed
     AdminLayoutComponent,
     AuthLayoutComponent,
     BlogComponent,
-    FeedBackComponent
+    FeedBackComponent,
+    RendezVousFeedbackComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/feedbackandreclamation/rendez-vous-feedback/rendez-vous-feedback.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rendez-vous-feedback',
+  templateUrl: './rendez-vous-feedback.component.html',
+  styleUrls: ['./rendez-vous-feedback.component.css']
+})
+export class RendezVousFeedbackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
